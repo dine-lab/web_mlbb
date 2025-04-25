99b78e7005fdac2b197aa5e869e69fda17fd958e
Dine <dgarcia78@cuc.edu.co>
2025-04-25 15:56:17 -0500

    Actualizado

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,5 @@
-import os
 import reflex as rx
 
-def main():
-    # Usa el puerto asignado por el entorno de Render
-    port = os.getenv("PORT", 3001)  # Si no encuentra la variable de entorno, usará 3001
-    rx.run(port=port)
-
-if __name__ == "__main__":
-    main()
+config = rx.Config(
+    app_name="Mlbb_web",
+)
